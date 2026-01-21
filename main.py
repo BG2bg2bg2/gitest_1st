b3a64ea432b2bdddc30226ccd57d34dfe4232194
BG2bg2bg2 <gerlachbrett@gmail.com>
2026-01-21 09:52:44 -0700

    Brett's starting stuff

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
-#Hi
+#Pseudocode
 
-#hi
+#display you are in the year 2044
+
+#inventory
+#store
+#stats/data
+    #display you are level (Changeable) and (changable) rank
+    #you have (inventory) items
+    #you have (changable) souls
+    #you have ()
+
+#start funtion
+#while true
+    #ask user to choose a character type (human, dog, goblin, bear)
+    #ask user what color they want for their skin/fur
+    #ask user what eye color they want
+    #ask user how tall they want to be
+    #ask user what hunter type they want to be
+    #update stats/data
+
+#store funtion
+#while true
+    #display money from inventroy
+    #display store items
+    #ask what item they want to buy
+    #if item cost is over money amount
+        #display too much for too little money
+    #else if item cost is under money amount
+        #subtract money from item amount
+        #remove item from store
+        #move item to inventory
+    #else
+        #input a valid number to get the item
+
+#data function
+#while true
+    #ask if user wants to see inventory or items in store or see their stats/data
+    #display/show inventory
+    #display/show store inventroy
+    #display stats/data
