4d74d9401f6e577484a2c25af423fbaa2eed1ece
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-01-21 09:52:35 -0700

    khgv

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #Hi
+
+
+
+
+# Ask user if they want to store anything or change anything 
+    #if the user choose to store anything 
+        # user can collect items or buy weapons, potions, charms, and key for gates
+    #if the user choose to change anything ask what they want to change
+        # user can set a soul free or use/drop an item
+            # if user 
