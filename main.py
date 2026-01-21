39e53217e78d1cd68c461af2f1bd057b677aa666
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-01-21 09:50:30 -0700

    Finished making the list for the characters

--- main.py
+++ main.py
@@ -15,4 +15,14 @@
 #if user has collected x amount of souls then
 # you have leveled up by one level
 # keep repeating until you have max level
-#
+
+#human = [ strength,health,skill,level ]
+# dog = [ strength,health,skill,level]
+# goblin  = [strength,health,skill ]
+# bear = [strength,health, skill, level]
+
+
+# if user had 10 <= kills
+#   show user you have unlcoked specail skill
+# add skill to list of characteristics
+# 
