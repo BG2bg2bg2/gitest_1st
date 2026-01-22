a582243181497be0e6776ccda9fde57cbf53da99
nicole12131 <neilybeth.salom@ucas-edu.net>
2026-01-22 09:26:02 -0700

    123456

--- main.py
+++ main.py
@@ -2,10 +2,19 @@
 
 
 
+# Make a dictionary that stores items
 
-# Ask user if they want to store anything or change anything 
+# Make a function for the inventory 
+
+# Ask user if they want to store anything or change anything or quit
+    
     #if the user choose to store anything 
         # user can collect items or buy weapons, potions, charms, and key for gates
+    
     #if the user choose to change anything ask what they want to change
         # user can set a soul free or use/drop an item
-            # if user 
+        
+            # if user choose set a soul free user can decided what soul they want to choose
+            # if user choose to use/drop an item user can decided what item they want to use/drop
+        
+    # if user choose to quit the inventory will quit and user can continue with the game
