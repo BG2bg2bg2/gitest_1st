cd12349679a35f37e76aff0c75b5e328464e774d
BG2bg2bg2 <gerlachbrett@gmail.com>
2026-01-26 09:49:01 -0700

    asdf

--- main.py
+++ main.py
@@ -1,74 +1,9 @@
-#function for main
-    #call stats/data
-    #if start function not called
-        #call start
-    #call store if wanted
-    #call data function if wanted
-    #call inventory function if wanted
-    #call power function if wanted
-    #go home to change character/heal
+#main menu
+#options user has
 
-
-#inventory
-#store
-#stats/data
-    #display you are level (Changeable) and (changable) rank
-    #you have (inventory) items
-    #you have (changable) souls
-    #you have ()
-
-
-
-
-#start function
-#while true
-    #ask user to choose a character type (human, dog, goblin, bear)
-    #ask user what color they want for their skin/fur
-    #ask user what eye color they want
-    #ask user how tall they want to be
-    #ask user what hunter type they want to be
-    #update stats/data
-
-
-
-
-#store function
-#while true
-    #display money from inventory
-    #display store items
-    #ask what item they want to buy
-    #if item cost is over money amount
-        #display too much for too little money
-    #else if item cost is under money amount
-        #subtract money from item amount
-        #remove item from store
-        #move item to inventory
-    #else
-        #input a valid number to get the item
-
-
-
-
-#data function
-#while true
-    #ask if user wants to see inventory or items in store or see their stats/data
-    #display/show inventory
-    #display/show store inventory
-    #display stats/data
-
-
-
-
-#power function
-    #if user wants to know what powers they have/wanted
-    #display the powers they have
-    #display the powers they could have
-    #display how to get powers they want
-    #display how to use the powers they have so far
-
-
-#home function
-    #display they are home
-    #ask if they want to eat
-    # ask if they want to sleep
-    # ask if they want to do anything
+#funtion for main menu
+    #do you want to change your character
+    #do you want to see what is in your inventory
+    #do you want to go through a toltorial to get to know your powers?
+    #do you want to know the ranks of everything around you?
+    
