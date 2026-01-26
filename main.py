f1a82691732b3ea157ed109e834509e6b24e7ccc
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-01-26 09:30:35 -0700

    main

--- main.py
+++ main.py
@@ -2,27 +2,38 @@
 
 
 
+#skills = []
+
+#level = 1
+
+# leveling up function
+    
+    #if user has collected 3  of souls then
+    # you have leveled up by one level 1 += level 
+    #while level < 10 
+    # keep adding a level for every three kills
+
+# making a function to view characters options and what skills they have
+#   storing all the characterics for each character in an induval list
+#   human = [ strength = 23, health = 50 ]
+#   dog = [ strength = 47 ,health= 120,]
+#   goblin  = [strength = 35 ,health= 100, ]
+#   bear = [strength = 12 ,health = 25 , ,]
 
 
 
 
 
-
-
-
-
-
-#if user has collected x amount of souls then
-# you have leveled up by one level
-# keep repeating until you have max level
-
-#human = [ strength,health,skill,level ]
-# dog = [ strength,health,skill,level]
-# goblin  = [strength,health,skill ]
-# bear = [strength,health, skill, level]
-
-
-# if user had 10 <= kills
-#   show user you have unlcoked specail skill
-# add skill to list of characteristics
-# 
+# making a function to handle skill dependencies and prerequisites(by using if statements)
+# list for special skills [flying monkey, flying dog,spin kick]
+    # if user had 10 == souls
+    #   show user you have unlocked special skill
+    #   pull one of the random skills from the list 
+    #   add it onto the skills
+    # elif user had 8 ==  souls
+        #show user you got a special skill
+        # its flying blobfish 
+        # add blobfish to SKILL LIST
+    # elif user had 5 == souls
+    #    how user you dont get a skill
+    #else print you just moved up a level
