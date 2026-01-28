e51eba80dd0fc556c6ba61c2146078eb46c4fdfe
IsCowdell <isabella.cowdell@ucas-edu.net>
2026-01-28 09:42:24 -0700

    main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Hi
 
-
+import random
 
 #skills = []
 skills = []
@@ -18,6 +18,7 @@ def function():
     #while level < 10 
     while level < 10:
     # keep adding a level for every three kills
+      level += 1 
         
 
 # making a function to view characters options and what skills they have
@@ -37,15 +38,30 @@ def characters_options():
 
 
 # making a function to handle skill dependencies and prerequisites(by using if statements)
+def skill_change():
 # list for special skills [flying monkey, flying dog,spin kick]
+  special_skills = ["flying monkey", "flying dog","spin kick,flying blobfish"]
     # if user had 10 == souls
+  if souls == 10: 
     #   show user you have unlocked special skill
-    #   pull one of the random skills from the list 
-    #   add it onto the skills
-    # elif user had 8 ==  souls
+    print("you have unlocked a special skills")
+    #   pull one of the random skills from the list
+    random_skill = random.choice(special_skills)
+    #add it onto the skills
+    skills.append(random_skill)
+  # else if user had eqaul 8 souls
+  elif souls == 8:
         #show user you got a special skill
+        print("you got a special skills")
         # its flying blobfish 
+        print("ITS FLYING BLOBFISH")
         # add blobfish to SKILL LIST
+        skills.append("flying blobfish")
+
     # elif user had 5 == souls
+  elif souls == 5:
     #    how user you dont get a skill
+    print("you dont get a skill")
     #else print you just moved up a level
+  else:
+     print("you don't get anything")
